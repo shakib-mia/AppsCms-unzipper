ce1663926c7bfb306d42645abf162de775f052fd
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-06-17 15:18:18 +0600

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const input = () => {
+    let getInput = document.createElement("input");
+    getInput.type = "file";
+    getInput.click();
+    getInput.addEventListener("change", (event) => {
+      console.log(event.target.files);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-slate-100 h-screen">
+      <div className="container w-1/2 mx-auto">
+        <h1 className="text-3xl font-bold text-center">Archive Extractor</h1>
+        <p className="text-center">
+          Archive Extractor is a small and easy online tool that can extract
+          over 70 types of compressed <br /> files, such as 7z, zipx, rar, tar,
+          exe, dmg and much more.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="mx-auto bg-white w-1/3 py-10">
+          <div id="upload-section" className="text-center">
+            <button
+              onClick={input}
+              type="file"
+              className="bg-blue-600 text-white px-5 py-3 rounded-md"
+            >
+              <div className="text-xl">Choose File</div>
+              <small className="text-light">From Your Computer</small>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
