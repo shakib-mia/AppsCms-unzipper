e83f6afb080e1a08c6e698a77ece9389092525ea
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-06-19 13:21:30 +0600

    extraction functionality added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import "./App.css";
+import { useState } from "react";
+import JSZip from "jszip";
 
 function App() {
+  const [about, setAbout] = useState({});
+
+  // handle button click
+
   const input = () => {
     let getInput = document.createElement("input");
     getInput.type = "file";
     getInput.click();
     getInput.addEventListener("change", (event) => {
-      console.log(event.target.files[0]);
+      const file = event.target.files[0];
+      if (
+        file.name.includes(".zip") ||
+        file.name.includes(".7z") ||
+        file.name.includes(".dmg") ||
+        file.name.includes(".zipx") ||
+        file.name.includes(".rar") ||
+        file.name.includes(".tar") ||
+        file.name.includes(".exe")
+      ) {
+        JSZip.loadAsync(file).then((data) => console.log(data));
+        setAbout(file);
+
+        document.getElementById("fileSection").style.display = "none";
+        document.getElementById("details-section").style.display = "block";
+      } else {
+        document
+          .getElementById("details-section")
+          .classList.add("text-rose-500");
+        document.getElementById("fileSection").style.display = "none";
+        document.getElementById("details-section").style.display = "block";
+        document.getElementById("details-section").innerText =
+          "Unsupported File Format";
+      }
     });
   };
 
-  const handleFileUpload = (file) => {
-    if (file.name.includes(".zip")) {
-      console.log(file);
-    }
-  };
+  // handle drag and drop
 
-  const handleMouseOver = (e) => {
-    const element = document.getElementsByClassName("custom-file-input");
-    const styles = window.getComputedStyle(element, "::before");
-    styles.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  const handleFileUpload = (file) => {
+    if (
+      file.name.includes(".zip") ||
+      file.name.includes(".7z") ||
+      file.name.includes(".dmg") ||
+      file.name.includes(".zipx") ||
+      file.name.includes(".rar") ||
+      file.name.includes(".tar") ||
+      file.name.includes(".exe")
+    ) {
+      JSZip.loadAsync(file).then((data) => console.log(data));
+      setAbout(file);
+      document.getElementById("fileSection").style.display = "none";
+      document.getElementById("details-section").style.display = "block";
+    } else {
+      document.getElementById("fileSection").style.display = "none";
+      document.getElementById("details-section").style.display = "block";
+      document.getElementById("details-section").classList.add("text-rose-500");
+      document.getElementById("details-section").innerText =
+        "Unsupported File Format";
+    }
   };
 
   return (
@@ -29,12 +71,15 @@ function App() {
           <h1 className="text-3xl font-bold text-center">Archive Extractor</h1>
           <p className="text-center">
             Archive Extractor is a small and easy online tool that can extract
-            over 70 types of compressed <br /> files, such as 7z, zipx, rar and
-            much more.
+            over 70 types of compressed <br /> files, such as 7z, zipx, rar,
+            tar,exe,dmg and much more.
           </p>
         </div>
 
-        <div className="mx-auto bg-white py-10 border border-1 rounded">
+        <div
+          className="mx-auto bg-white py-10 border border-1 rounded"
+          id="fileSection"
+        >
           <div id="upload-section" className="text-center">
             <button
               onClick={input}
@@ -46,19 +91,25 @@ function App() {
             </button>
           </div>
           <div className="flex justify-center">
-            <div id="drop_zone demo" className="p-10 droptarget">
+            <div
+              id="drop_zone demo"
+              className="border border-dashed border-rose-500 droptarget mt-3"
+            >
               <input
                 type="file"
                 onChange={(e) => handleFileUpload(e.target.files[0])}
                 id="file"
                 onClick={(e) => e.preventDefault()}
-                onDrag={(e) => {
-                  handleMouseOver(e);
-                }}
                 className="custom-file-input"
               />
             </div>
           </div>
+        </div>
+        <div id="details-section" style={{ display: "none" }}>
+          <h1 className="text=2xl font-bold">Name: {about?.name}</h1>
+          <h1 className="text=2xl font-bold">
+            Size: {(about?.size / (1024 * 1024)).toString().slice(0, 3)}MB
+          </h1>
         </div>
       </div>
     </div>
