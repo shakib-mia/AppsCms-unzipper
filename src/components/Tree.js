f4860181a00cf5bcf2f1b2ce2fc5a68a86c95bc9
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-06-22 21:05:13 +0600

    more icons added

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -20,17 +20,40 @@ const TreeNode = ({ node }) => {
     );
   }, [childs]);
 
-  console.log(childs.map((item) => item.split(".")));
+  const caret = document.getElementById("caret");
+  let getInput = document.getElementById("check");
+  if (getInput) {
+    getInput.style.display = "none";
+  }
+
+  const toggle = () => {
+    getInput.click();
+    caret.style.transform = "rotate(-45deg)";
+    document.getElementById("files").style.display = "none";
+
+    if (getInput.checked) {
+      caret.style.transform = "rotate(0deg)";
+      document.getElementById("files").style.display = "block";
+    }
+  };
 
   return (
     <div>
-      <span id="parent">
-        <i class="fa-solid fa-caret-down"></i>{" "}
+      <input type="checkbox" id="check" />
+      <span
+        id="parent"
+        onClick={toggle}
+        className="hover:bg-green-200 cursor-pointer"
+      >
+        <i class="fa-solid fa-caret-down" id="caret"></i>{" "}
         <i class="fa-solid fa-folder"></i> {childs[0].split("/")[0]}
       </span>
-      <ul className="ml-10" id="files">
+      <ul id="files">
         {childs.map((item) => (
-          <li key={childs.indexOf(item)}>
+          <li
+            key={childs.indexOf(item)}
+            className="hover:bg-green-100 cursor-pointer pl-4 flex items-center"
+          >
             {item.split(".")[1] === "js" || item.split(".")[2] === "js" ? (
               <i class="fa-brands mr-4 fa-js-square"></i>
             ) : item.split(".")[1] === "html" ||
@@ -49,6 +72,24 @@ const TreeNode = ({ node }) => {
               <i class="fa-regular mr-4 fa-file-image"></i>
             ) : item.split(".")[2] === "gif" ? (
               <i class="fa-thin fa-gif"></i>
+            ) : item.split(".")[1] === "md" ? (
+              <img
+                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/md-file-1932190-1635753.png"
+                alt="icon"
+                className="w-5 h-5 mr-4"
+              />
+            ) : item.split(".")[1] === "json" ? (
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA2qxrpBqZDPi7_zPqc5GzZCHO3N328jBXONV2TUzRMO0EfBQ2DpsGbDtmPaKWQg-3pN4&usqp=CAU"
+                alt="icon"
+                className="w-5 h-5 mr-4"
+              />
+            ) : item.split(".")[1] === "gitignore" ? (
+              <img
+                src="https://rubbersheep.gallerycdn.vsassets.io/extensions/rubbersheep/gi/0.2.11/1494307055124/Microsoft.VisualStudio.Services.Icons.Default"
+                alt="icon"
+                className="w-5 h-5 mr-4"
+              />
             ) : item.split(".")[1] === "css" ? (
               <i class="fa-brands fa-css3 mr-4"></i>
             ) : (
