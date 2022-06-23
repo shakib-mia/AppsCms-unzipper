228ab051bf7b56476da80fd0ed69c8d2a05d18d3
Abdullah Al Samad <smdshakibmia2001@gmail.com>
2022-06-23 09:44:12 +0600

    download functionality added

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -28,7 +28,7 @@ const TreeNode = ({ node }) => {
 
   const toggle = () => {
     getInput.click();
-    caret.style.transform = "rotate(-45deg)";
+    caret.style.transform = "rotate(-90deg)";
     document.getElementById("files").style.display = "none";
 
     if (getInput.checked) {
@@ -52,50 +52,54 @@ const TreeNode = ({ node }) => {
         {childs.map((item) => (
           <li
             key={childs.indexOf(item)}
-            className="hover:bg-green-100 cursor-pointer pl-4 flex items-center"
+            className="hover:bg-green-100 cursor-pointer pl-4"
           >
-            {item.split(".")[1] === "js" || item.split(".")[2] === "js" ? (
-              <i class="fa-brands mr-4 fa-js-square"></i>
-            ) : item.split(".")[1] === "html" ||
-              item.split(".")[2] === "html" ||
-              item.split(".")[2] === "htm" ? (
-              <i class="fa-solid mr-4 fa-code"></i>
-            ) : item.split(".")[1] === "pdf" || item.split(".")[2] === "pdf" ? (
-              <i class="fa-solid mr-4 fa-file-pdf"></i>
-            ) : item.split(".")[1] === "scss" ||
-              item.split(".")[1] === "_sass" ? (
-              <i class="fa-brands mr-4 fa-sass"></i>
-            ) : item.split(".")[1] === "png" ||
-              item.split(".")[1] === "jpg" ||
-              item.split(".")[2] === "png" ||
-              item.split(".")[2] === "jpg" ? (
-              <i class="fa-regular mr-4 fa-file-image"></i>
-            ) : item.split(".")[2] === "gif" ? (
-              <i class="fa-thin fa-gif"></i>
-            ) : item.split(".")[1] === "md" ? (
-              <img
-                src="https://cdn.iconscout.com/icon/premium/png-256-thumb/md-file-1932190-1635753.png"
-                alt="icon"
-                className="w-5 h-5 mr-4"
-              />
-            ) : item.split(".")[1] === "json" ? (
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA2qxrpBqZDPi7_zPqc5GzZCHO3N328jBXONV2TUzRMO0EfBQ2DpsGbDtmPaKWQg-3pN4&usqp=CAU"
-                alt="icon"
-                className="w-5 h-5 mr-4"
-              />
-            ) : item.split(".")[1] === "gitignore" ? (
-              <img
-                src="https://rubbersheep.gallerycdn.vsassets.io/extensions/rubbersheep/gi/0.2.11/1494307055124/Microsoft.VisualStudio.Services.Icons.Default"
-                alt="icon"
-                className="w-5 h-5 mr-4"
-              />
-            ) : item.split(".")[1] === "css" ? (
-              <i class="fa-brands fa-css3 mr-4"></i>
-            ) : (
-              ""
-            )}
-            {item.split("/").pop()}
+            <a href={item} download className="flex items-center">
+              {item.split(".")[item.split(".").length - 1] === "js" ||
+              item.split(".")[item.split(".").length - 1] === "js" ? (
+                <i class="fa-brands mr-4 fa-js-square"></i>
+              ) : item.split(".")[item.split(".").length - 1] === "html" ||
+                item.split(".")[item.split(".").length - 1] === "htm" ? (
+                <i class="fa-solid mr-4 fa-code"></i>
+              ) : item.split(".")[item.split(".").length - 1] === "pdf" ||
+                item.split(".")[item.split(".").length - 1] === "pdf" ? (
+                <i class="fa-solid mr-4 fa-file-pdf"></i>
+              ) : item.split(".")[item.split(".").length - 1] === "scss" ||
+                item.split(".")[item.split(".").length - 1] === "_sass" ? (
+                <i class="fa-brands mr-4 fa-sass"></i>
+              ) : item.split(".")[item.split(".").length - 1] === "png" ||
+                item.split(".")[item.split(".").length - 1] === "jpg" ||
+                item.split(".")[item.split(".").length - 1] === "png" ||
+                item.split(".")[item.split(".").length - 1] === "jpg" ? (
+                <i class="fa-regular mr-4 fa-file-image"></i>
+              ) : item.split(".")[item.split(".").length - 1] === "gif" ? (
+                <i class="fa-thin fa-gif"></i>
+              ) : item.split(".")[item.split(".").length - 1] === "md" ? (
+                <img
+                  src="https://cdn.iconscout.com/icon/premium/png-256-thumb/md-file-1932190-1635753.png"
+                  alt="icon"
+                  className="w-5 h-5 mr-4"
+                />
+              ) : item.split(".")[item.split(".").length - 1] === "json" ? (
+                <img
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA2qxrpBqZDPi7_zPqc5GzZCHO3N328jBXONV2TUzRMO0EfBQ2DpsGbDtmPaKWQg-3pN4&usqp=CAU"
+                  alt="icon"
+                  className="w-5 h-5 mr-4"
+                />
+              ) : item.split(".")[item.split(".").length - 1] ===
+                "gitignore" ? (
+                <img
+                  src="https://rubbersheep.gallerycdn.vsassets.io/extensions/rubbersheep/gi/0.2.11/1494307055124/Microsoft.VisualStudio.Services.Icons.Default"
+                  alt="icon"
+                  className="w-5 h-5 mr-4"
+                />
+              ) : item.split(".")[item.split(".").length - 1] === "css" ? (
+                <i class="fa-brands fa-css3 mr-4"></i>
+              ) : (
+                ""
+              )}
+              {item.split("/").pop()}
+            </a>
           </li>
         ))}
       </ul>
